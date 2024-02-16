1b576fa748273ceb4f30173d59c7a33a0e1a7226
vemulathrinath <thrinath.vemula@gmail.com>
2024-02-16 12:03:54 +0530

    Added 3 Test Case

--- pytestCases/test_login.py
+++ pytestCases/test_login.py
@@ -10,3 +10,9 @@ def test_login():
 def test_logout():
     print("Running the second test case")
     print("Logging out")
+
+
+def test_login_as():
+    print("Running the third test case")
+    print("Logging as Sushma Devi")
+    print("I love Sushma Devi")
