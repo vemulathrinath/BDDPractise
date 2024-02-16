39b682616fed1bbfe7cdfb66f15c6caf0fd496da
vemulathrinath <thrinath.vemula@gmail.com>
2024-02-16 11:55:06 +0530

    Added 2 Test Cases

---
+++ pytestCases/test_login.py
@@ -0,0 +1,12 @@
+import pytest
+
+
+def test_login():
+    # driver.get("http:")
+    print("Running the first test case")
+    print("Logging in")
+
+
+def test_logout():
+    print("Running the second test case")
+    print("Logging out")
